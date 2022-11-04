57c0349a6e9cad11d4d59727cd82e9be2370bf04
Nevil Sandaradura <nevil.sandaradura@gmail.com>
2022-11-04 22:46:35 +0200

    added router to app page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom'
 import TestPage from './pages/testpage';
-import BlueButton from './components/BlueButton/BlueButton'
 
 const App = () => {
   return (
     <div className="App">
-      <div>
-        <BlueButton text='Join this order'/>
-      </div>
+      <Routes>
+        <Route path="/" caseSensitive={false} element={<TestPage />} />
+      </Routes>
     </div >
   );
 }
