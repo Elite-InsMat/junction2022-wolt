ffe9928f33020656d1327b4da9bec0ff555f6ee4
Juhoton <juho.k.nykanen@hotmail.com>
2022-11-04 22:04:15 +0200

    Restaurant component added

--- frontend/src/pages/testpage.tsx
+++ frontend/src/pages/testpage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import TextComponent from "../components/TextComponent";
+import RestaurantComponent from "../components/RestaurantComponent";
 
 
 
@@ -7,7 +8,8 @@ import TextComponent from "../components/TextComponent";
 const TestPage = () => {
     return (
         <div>
-            <TextComponent text="Ääääääääää" />
+            <RestaurantComponent text="aaaaa" img="https://i.media.fi/incoming/ujb6vi/7260078.jpg/alternates/FREE_1440/7260078.jpg"/>
+            <RestaurantComponent text="eeeee" img="https://upload.wikimedia.org/wikipedia/commons/8/8c/Cow_%28Fleckvieh_breed%29_Oeschinensee_Slaunger_2009-07-07.jpg"/>
         </div>
     )
 
