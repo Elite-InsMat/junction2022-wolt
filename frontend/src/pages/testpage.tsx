71798a2c532b0776dff53d587869bae15c511e8b
Nevil Sandaradura <nevil.sandaradura@gmail.com>
2022-11-04 23:31:34 +0200

    fixed

--- frontend/src/pages/testpage.tsx
+++ frontend/src/pages/testpage.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import RestaurantComponent from "../components/RestaurantComponent";
 
 
-import TextComponent from "../components/TextComponent";
-import RestaurantOrderOuterContainerBox from "../components/RestaurantOrderOuterContainerBox/RestaurantOrderOuterContainerBox";
 
 
 const TestPage = () => {
@@ -11,7 +9,6 @@ const TestPage = () => {
         <div>
             <RestaurantComponent text="aaaaa" img="https://i.media.fi/incoming/ujb6vi/7260078.jpg/alternates/FREE_1440/7260078.jpg" />
             <RestaurantComponent text="eeeee" img="https://upload.wikimedia.org/wikipedia/commons/8/8c/Cow_%28Fleckvieh_breed%29_Oeschinensee_Slaunger_2009-07-07.jpg" />
-            <RestaurantOrderOuterContainerBox />
         </div>
     )
 
