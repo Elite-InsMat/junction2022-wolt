5767d21aa9e21966cd7d3d495acf7c04f4330b4a
Patrik Larsen <patrik@patriklarsen.com>
2022-11-05 00:22:53 +0200

    order page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom'
 import { FrontPage } from './pages/Frontpage';
+import OrderPage from './pages/order-page/order-page';
 import TestPage from './pages/testpage';
 
 const App = () => {
@@ -8,8 +9,8 @@ const App = () => {
     <div className="App">
       <Routes>
         <Route path="/test" caseSensitive={false} element={<TestPage />} />
+        <Route path="/order" caseSensitive={false} element={<OrderPage />} />
         <Route path="/" caseSensitive={false} element={<FrontPage />} />
-
       </Routes>
     </div >
   );
